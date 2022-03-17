57f954bcd53728acc7c18cf4c3e1aae1e45d57e1
Matias Esteban <estebanmatias92@gmail.com>
2022-03-16 21:15:17 -0300

    Fixing zsh_syntax_highlighting plugin path

--- zshconfig.sh
+++ zshconfig.sh
@@ -30,7 +30,7 @@ echo "-----------------------------------------------------------------"
 echo "------ Installing Oh-My-Zsh plugin for syntax highlighting ------"
 echo "-----------------------------------------------------------------"
 echo ""
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sleep 1
 
 
